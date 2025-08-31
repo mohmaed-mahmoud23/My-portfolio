7592203987721c76b600116514e83e96fac3fbf9
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-08-31 23:54:50 +0000

    Add A New projects

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -16,10 +16,28 @@ const projects = [
     link: "https://iphone-website-kappa.vercel.app/",
   },
   {
-    name: "simple project",
+    name: " vs code clone",
     align: "right",
-    image: "/images/b566ddb4-2511-4dc6-89d0-5ff73023cfc2.jpg", // ✅ مسار صحيح
-    link: "https://simpel-progect-fp9e.vercel.app/",
+    image: "/images/WhatsApp Image 2025-08-31 at 8.27.58 PM.jpeg", // ✅ مسار صحيح
+    link: "https://vs-clone-three.vercel.app/",
+  },
+  {
+    name: " product belder",
+    align: "left",
+    image: "/images/WhatsApp Image 2025-08-31 at 8.30.26 PM.jpeg", // ✅ مسار صحيح
+    link: "https://prudact-belder.vercel.app/",
+  },
+  {
+    name: " Super-Pro – Business Website",
+    align: "right",
+    image: "/images/WhatsApp Image 2025-08-31 at 8.42.21 PM.jpeg", // ✅ مسار صحيح
+    link: "https://super-pro-frontend.vercel.app/ ",
+  },
+  {
+    name: "Auto-Store – Business Website  ",
+    align: "left",
+    image: "/images/WhatsApp Image 2025-08-31 at 8.47.02 PM.jpeg", // ✅ مسار صحيح
+    link: "https://auto-store-landing-page.vercel.app/",
   },
 ];
 
